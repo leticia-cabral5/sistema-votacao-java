8cd8709122ff9e5a630dd643ed778db3f301dc8f
Letícia Cabral Pinheiro Soares <leticia.8212320@aluno.mg.gov.br>
2026-08-12 10:03:48 -0300

    chore: criar estrutura inicial do programa

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class SistemaVotacao {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Sistema de Votação");
+        System.out.println("Sistema de votação iniciado.");
+        scanner.close();
     }
 }
